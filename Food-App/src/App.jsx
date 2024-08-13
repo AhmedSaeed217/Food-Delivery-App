e7a5905fa8826fa8991f23e562b0671ae10ac61f
AhmedSaeed <ahmedsaeedfci@gmail.com>
2024-08-13 21:49:38 +0300

    Modified Application Files

--- Food-App/src/App.jsx
+++ Food-App/src/App.jsx
@@ -6,20 +6,20 @@ import Cart from './Pages/Cart/Cart';
 import Placeorder from './Pages/Placeorder/Placeorder';
 
 function App() {
+  
 
-
-  return <>
-    <div className="app">
-      <Navbar />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/cart' element={<Cart />} />
-        <Route path='/order' element={<Placeorder />} />
-      </Routes>
-    </div>
-
-  </>
-
+  return  <>
+     <div className="app">
+     <Navbar/>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/cart' element={<Cart/>}/>
+      <Route path='/order' element={<Placeorder/>}/>
+    </Routes>
+     </div>
+       
+    </>
+  
 }
 
 export default App;
--- Food-App/src/Components/Navbar/Navbar.jsx
+++ Food-App/src/Components/Navbar/Navbar.jsx
@@ -1,52 +1,52 @@
-import React from "react";
+// import React from "react";
 import './Navbar.css';
-import { assets } from '../../assets/assets';
+import {assets} from '../../assets/assets';
 import { useState } from 'react';
 export default function Navbar() {
 
-    const [activeLink, setActiveLink] = useState('home');
+const [activeLink,setActiveLink]=useState('home');
 
 
 
-    return <>
+  return <>
 
-        <div className="navbar">
+<div className="navbar">
 
-            <div className="left-navbar">
-                <img src={assets.logo} className="logo" alt="" />
-            </div>
-            <div className="center-navbar">
-                <ul className="navbar-menu">
-                    <li onClick={() => setActiveLink('home')}
-                        className={activeLink === 'home' ? "active" : ''}>Home</li>
-                    <li onClick={() => setActiveLink('menu')}
-                        className={activeLink === 'menu' ? "active" : ''}>Menu</li>
-                    <li onClick={() => setActiveLink('mobileapp')}
-                        className={activeLink === 'mobileapp' ? "active" : ''}>Mobile-app</li>
-                    <li onClick={() => setActiveLink('contact')}
-                        className={activeLink === 'contact' ? "active" : ''}>Contact Us</li>
-                </ul>
-            </div>
-            <div className="right-navbar">
-                <div className="search-icon">
-                    <img src={assets.search_icon} alt="" />
-                </div>
-                <div className="cart-icon">
-                    <img src={assets.basket_icon} alt="" />
-                    <div className="cart-items-number">0</div>
-                </div>
-                <div className="singup-button">
-                    <button>Signup</button>
-                </div>
-            </div>
-
-
-
-
-
-
+    <div className="left-navbar">
+        <img src={assets.logo} className="logo" alt="" />
+    </div>
+    <div className="center-navbar">
+        <ul className="navbar-menu">
+            <li onClick={()=>setActiveLink('home')}
+             className={activeLink==='home'?"active":''}>Home</li>
+            <li onClick={()=>setActiveLink('menu')}
+            className={activeLink==='menu'?"active":''}>Menu</li>
+            <li onClick={()=>setActiveLink('mobileapp')}
+             className={activeLink==='mobileapp'?"active":''}>Mobile-app</li>
+            <li onClick={()=>setActiveLink('contact')}
+            className={activeLink==='contact'?"active":''}>Contact Us</li>
+        </ul>
+    </div>
+    <div className="right-navbar">
+        <div className="search-icon">
+            <img src={assets.search_icon} alt="" />
         </div>
+        <div className="cart-icon">
+            <img src={assets.basket_icon} alt="" />
+            <div className="cart-items-number">0</div>
+        </div>
+        <div className="singup-button">
+            <button>Signup</button>
+        </div>
+    </div>
 
 
-    </>
+
+
+
+
+</div>
+
+
+  </>
 }
